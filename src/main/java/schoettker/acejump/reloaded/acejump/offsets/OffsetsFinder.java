535e3c160f29f399eea5c1cdc80ea377c4ff4507
danys <dany.sery@seznam.cz>
2025-10-10 11:20:10 +0200

    Added word end offsets

--- src/main/java/schoettker/acejump/reloaded/acejump/offsets/OffsetsFinder.java
+++ src/main/java/schoettker/acejump/reloaded/acejump/offsets/OffsetsFinder.java
@@ -3,10 +3,8 @@ package schoettker.acejump.reloaded.acejump.offsets;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.util.TextRange;
-import schoettker.acejump.reloaded.options.PluginConfig;
 import schoettker.acejump.reloaded.util.EditorUtils;
 
-import java.awt.event.KeyEvent;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,7 +13,9 @@ public class OffsetsFinder {
         Document document = editor.getDocument();
         TextRange visibleRange = EditorUtils.getVisibleTextRange(editor);
 
-        return getWordStartOffsets(visibleRange, document);
+        List<Integer> offsets = getWordEndOffsets(visibleRange, document);
+        addOffsetsOfChar(visibleRange.getStartOffset(), '\n', document.getText(visibleRange), editor, offsets);
+        return offsets;
 //        List<Integer> offsets = getOffsetsOfCharIgnoreCase(String.valueOf(key), visibleRange, document, editor, selectedEditor);
 //
 //        if (key == KeyEvent.VK_SPACE) {
@@ -37,26 +37,9 @@ public class OffsetsFinder {
         }
     }
 
-    private ArrayList<Integer> getOffsetsOfCharIgnoreCase(String charSet, TextRange markerRange, Document document, Editor editor, Editor selectedEditor) {
-        ArrayList<Integer> offsets = new ArrayList<Integer>();
-        String visibleText = document.getText(markerRange);
-
-        for (char charToFind : charSet.toCharArray()) {
-            char lowCase = Character.toLowerCase(charToFind);
-            char upperCase = Character.toUpperCase(charToFind);
-
-            offsets.addAll(getOffsetsOfChar(markerRange.getStartOffset(), lowCase, visibleText, editor, selectedEditor));
-            if (upperCase != lowCase) {
-                offsets.addAll(getOffsetsOfChar(markerRange.getStartOffset(), upperCase, visibleText, editor, selectedEditor));
-            }
-        }
-
-        return offsets;
-    }
-
-    private ArrayList<Integer> getOffsetsOfChar(int startOffset, char c, String visibleText, Editor editor, Editor selectedEditor) {
+    private void addOffsetsOfChar(int startOffset, char c, String visibleText,
+                                                Editor editor, List<Integer> offsets) {
         int caretOffset = editor.getCaretModel().getOffset();
-        ArrayList<Integer> offsets = new ArrayList<>();
 
         int index = visibleText.indexOf(c);
         while (index >= 0) {
@@ -68,8 +51,6 @@ public class OffsetsFinder {
 
             index = visibleText.indexOf(c, index + 1);
         }
-
-        return offsets;
     }
 
     protected boolean isValidOffset(char c, String visibleText, int index, int offset, int caretOffset) {
@@ -108,5 +89,39 @@ public class OffsetsFinder {
         }
 
         return wordStartOffsets;
+    }
+
+    /**
+     * Gets the offsets where words end within the visible text range.
+     * A word end is defined as the position of the last character of a word,
+     * where words are sequences of alphanumeric characters and underscores separated by whitespace or punctuation.
+     *
+     * @param textRange the text range to search within
+     * @param document the document to search in
+     * @return List of offsets where words end
+     */
+    public List<Integer> getWordEndOffsets(TextRange textRange, Document document) {
+        List<Integer> wordEndOffsets = new ArrayList<>();
+        String visibleText = document.getText(textRange);
+        int startOffset = textRange.getStartOffset();
+
+        for (int i = 0; i < visibleText.length(); i++) {
+            char currentChar = visibleText.charAt(i);
+
+            // Check if current character is a word character (letter, digit, or underscore)
+            if (Character.isJavaIdentifierStart(currentChar) || Character.isJavaIdentifierPart(currentChar)) {
+
+                // Check if this is the end of a word
+                boolean isWordEnd = (i == visibleText.length() - 1) ||
+                    !(Character.isJavaIdentifierStart(visibleText.charAt(i + 1)) ||
+                      Character.isJavaIdentifierPart(visibleText.charAt(i + 1)));
+
+                if (isWordEnd) {
+                    wordEndOffsets.add(startOffset + i + 1);
+                }
+            }
+        }
+
+        return wordEndOffsets;
     }
 }
